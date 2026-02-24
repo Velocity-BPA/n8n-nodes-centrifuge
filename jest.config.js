c5e023cdd76418dfb89de0b894d83cc025e73289
John Drefahl <john@velobpa.com>
2026-02-24 14:04:45 -0600

    chore: update jest.config.js to v1.0.0

--- jest.config.js
+++ jest.config.js
@@ -1,40 +1,24 @@
 /*
- * Copyright (c) Velocity BPA, LLC
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	roots: ['<rootDir>/test'],
-	testMatch: ['**/*.test.ts'],
-	moduleFileExtensions: ['ts', 'js', 'json'],
-	collectCoverageFrom: [
-		'nodes/**/*.ts',
-		'credentials/**/*.ts',
-		'!**/*.d.ts',
-		'!**/index.ts',
-	],
-	coverageDirectory: 'coverage',
-	coverageReporters: ['text', 'lcov', 'html'],
-	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/$1',
-	},
-	testTimeout: 30000,
-	verbose: true,
-	globals: {
-		'ts-jest': {
-			tsconfig: {
-				module: 'commonjs',
-				target: 'es2022',
-				esModuleInterop: true,
-				allowSyntheticDefaultImports: true,
-				strict: true,
-				skipLibCheck: true,
-				types: ['jest', 'node'],
-			},
-		},
-	},
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  collectCoverageFrom: [
+    'nodes/**/*.ts',
+    'credentials/**/*.ts',
+    '!**/*.d.ts',
+    '!**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\.ts$': 'ts-jest',
+  },
+  testTimeout: 30000,
 };
